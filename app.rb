270ac11a4aeaaa3fdc75ff67b50cbdf2c3ddada8
Christopher Dent <christopherdent01@gmail.com>
2019-12-01 21:39:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
 
  post '/food' do 
-   "My name is #{params[:name]} and I like #{params[:favorite_food]}"
+   "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
    # params.to_s 
  end
 
